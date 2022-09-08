f649ed4c7e19cbcca86069c19da69443c94e8873
konmp <2463713831@qq.com>
2022-09-08 20:06:23 +0800

    RabbitMq学习实例

--- src/main/java/com/example/mapStudy/consume/DirectReceiver.java
+++ src/main/java/com/example/mapStudy/consume/DirectReceiver.java
@@ -1,18 +1,12 @@
 package com.example.mapStudy.consume;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-/**
- * 定义消费端
- * @BelongsProject: mapStudy
- * @BelongsPackage: com.example.mapStudy.consume
- * @Author: 99451
- * @CreateTime: 2022-09-06  23:06
- * @Description: TODO
- * @Version: 1.0
- */
+
+/*
 @Component
 @Log4j2
 public class DirectReceiver {
@@ -20,5 +14,20 @@ public class DirectReceiver {
 //    @RabbitListener(queues= "#{directQueue.name}")
     public void recvOne(String obj) {
         log.info("DirectReceiver, receive the queue msg:"+obj);
+    }
+}*/
+
+/**
+ *监听的队列名称DirectQueue
+ * 消费者
+ * @author mapei
+ */
+@Component
+//@RabbitListener(queues = "MapQueue")
+public class DirectReceiver {
+//    @RabbitHandler
+    public void receiver(String message) {
+
+        System.out.println("DirectReceiver消费者收到消息: " + message.toString());
     }
 }
--- src/main/java/com/example/mapStudy/provider/RabbitDirectSender.java
+++ src/main/java/com/example/mapStudy/provider/RabbitDirectSender.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
