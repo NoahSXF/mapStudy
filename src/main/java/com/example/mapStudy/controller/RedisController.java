0c012c9c477fe7449c956d97eb55f2b91743a111
NoahSXF <994518231@qq.com>
2022-09-10 21:49:32 +0800

    RedisController

---
+++ src/main/java/com/example/mapStudy/controller/RedisController.java
@@ -0,0 +1,38 @@
+package com.example.mapStudy.controller;
+
+import com.example.mapStudy.bean.Car;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 熟悉Redis的常用命令
+ *
+ * @BelongsProject: mapStudy
+ * @BelongsPackage: com.example.mapStudy.controller
+ * @Author: Noah
+ * @CreateTime: 2022-09-10  21:46
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/selectRedisObject")
+    public Map<String, Object> selectRedisObject() {
+        Map<String, Object> map = new HashMap<>(16);
+        Car car = new Car("东风", "SUV");
+        redisTemplate.opsForValue().set("car", car);
+        Object o = redisTemplate.opsForValue().get("car");
+        map.put("car", o);
+        return map;
+    }
+}
