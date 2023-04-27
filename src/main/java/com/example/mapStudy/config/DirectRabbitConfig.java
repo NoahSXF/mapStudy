4a626f913a3e7f40cd04495e5e0b5fb706abaa5d
konmp <2463713831@qq.com>
2023-04-27 22:37:45 +0800

    RabbitMq学习实例1

--- src/main/java/com/example/mapStudy/config/DirectRabbitConfig.java
+++ src/main/java/com/example/mapStudy/config/DirectRabbitConfig.java
@@ -3,7 +3,6 @@ package com.example.mapStudy.config;
 import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 配置队列
@@ -22,7 +21,7 @@ public class DirectRabbitConfig {
     public Queue smsQueue() {
         // durable:是否持久化,默认是false,持久化队列：会被存储在磁盘上，当消息代理重启时仍然存在，暂存队列：当前连接有效
         // exclusive:默认也是false，只能被当前创建的连接使用，而且当连接关闭后队列即被删除。此参考优先级高于durable
-        // autoDelete:是否自动删除，当没有生产者或者消费者使用此队列，该队列会自动删除。
+        //        // autoDelete:是否自动删除，当没有生产者或者消费者使用此队列，该队列会自动删除。
         //return new Queue("DirectQueue",true,true,false);
 
         //一般设置一下队列的持久化就好,其余两个就是默认false
